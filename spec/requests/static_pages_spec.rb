3790c4d58bdb4b739da18278f468e29e172db747
Matthias Schönborn <matthias@nandu.net>
2013-11-10 16:38:47 +0100

    Finish static pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,43 @@
+require 'spec_helper'
+
+describe "Static pages" do
+
+  describe "Home page" do
+
+    it "should have the content 'Mandarin Banana'" do
+      visit '/static_pages/home'
+      expect(page).to have_content('Mandarin Banana')
+    end
+
+    it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Mandarin Banana | Home")
+    end
+  end
+
+  describe "Help page" do
+
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_content('Help')
+    end
+
+    it "should have the title 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_title("Mandarin Banana | Help")
+    end
+  end
+
+  describe "About page" do
+
+    it "should have the content 'About Mandarin Banana'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('About Mandarin Banana')
+    end
+
+    it "should have the title 'About Mandarin Banana'" do
+      visit '/static_pages/about'
+      expect(page).to have_title("Mandarin Banana | About Mandarin Banana")
+    end
+  end
+end
