fd229e3f5d25409a770ae369f9b40905f3b09f4f
Matthias Schönborn <matthias@nandu.net>
2014-05-26 21:57:52 +0200

    Add hanzi editing

--- app/controllers/hanzis_controller.rb
+++ app/controllers/hanzis_controller.rb
@@ -1,4 +1,6 @@
 class HanzisController < ApplicationController
+  before_action :admin_user, only: [:edit, :update]
+
   def index
     if params[:hanzi_search]
       @hanzi = Hanzi.find_by(character: "#{params[:hanzi_search]}")
@@ -20,4 +22,28 @@ class HanzisController < ApplicationController
     @topic = "hanzi"
     @topic_id = @hanzi.id
   end
+
+  def edit
+    @hanzi = Hanzi.find(params[:id])
+  end
+
+  def update
+    @hanzi = Hanzi.find(params[:id])
+    if @hanzi.update_attributes(hanzi_params)
+      flash[:success] = "Hanzi updated"
+      redirect_to @hanzi
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+    def hanzi_params
+      params.require(:hanzi).permit(:components)
+    end
+
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end
 end
