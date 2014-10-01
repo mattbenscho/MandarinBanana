98362d0ded239fbf6f5194916a5c003f30056222
Matthias Schönborn <matthias@nandu.net>
2014-10-01 22:18:45 +0200

    Pinyin input review release

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,6 @@
 class ReviewsController < ApplicationController
   before_action :signed_in_user
-  before_action :correct_user, only: [:show, :answer, :fail, :fail_action, :validate, :update, :destroy]
+  before_action :correct_user, only: [:show, :answer, :fail, :fail_action, :update, :destroy]
 
   def index 
     @next = current_user.reviews.first
@@ -29,7 +29,11 @@ class ReviewsController < ApplicationController
   def show
     @review = current_user.reviews.find_by(id: params[:id])
     @hanzi = Hanzi.find_by(id: @review.hanzi_id)
-    @subtitles = @hanzi.subtitles.limit(7)
+    @random = @hanzi.subtitles.count
+    if @random > 0
+      @subtitle = @hanzi.subtitles.sample
+      @file = @subtitle.filename + ".ogv"
+    end
     @reviewed_today = Review.where("updated_at >= ? AND user_id = ?", Date.today, current_user.id).count
     @due_reviews_count = Review.where("due < ? AND user_id = ?", Time.now + 1.hours, current_user.id).count
     if current_user.reviews.count != 0
@@ -41,34 +45,73 @@ class ReviewsController < ApplicationController
 
   def answer
     @review = current_user.reviews.find_by(id: params[:id])
-    @hanzi = Hanzi.find_by(id: @review.hanzi_id)
-    @subtitles = @hanzi.subtitles.limit(7)
-    @appearances = Hanzi.where('components LIKE ?', "%#{@hanzi.character}%")
-    @stroke_order = check_stroke_order(@hanzi.character)
-    @comments = @hanzi.comments
-  end
-
-  def fail
-    @review = current_user.reviews.find_by(id: params[:id])
-    @hanzi = Hanzi.find_by(id: @review.hanzi_id)
-    @appearances = Hanzi.where('components LIKE ?', "%#{@hanzi.character}%")
-    @reviewed_appearances_counter = 0
-    @unknown_appearances = ""
-    @appearances.each do |a|
-      @is_being_reviewed = Review.find_by(hanzi_id: a.id)
-      if @is_being_reviewed.nil?
-        @unknown_appearances += a.character
-      else
-        @reviewed_appearances_counter += 1
+    counter = 0
+    to_fail = []
+    if !params[:subtitle_id].nil?
+      @subtitle = Subtitle.find(params[:subtitle_id])
+      @subtitle.sentence.each_char do |char|
+        counter += 1
+        @answer = params["selection" + counter.to_s]
+        if !@answer.nil?
+          @sentence_hanzi = Hanzi.find_by(character: char)
+          if !@sentence_hanzi.nil?
+            if @answer == @sentence_hanzi.character
+              validate(current_user.reviews.find_by(hanzi_id: @sentence_hanzi.id))
+            else
+              to_fail << current_user.reviews.find_by(hanzi_id: @sentence_hanzi.id).id
+            end
+          end
+        end
       end
+    else
+      counter += 1
+      @answer = params["selection1"]
+      @hanzi = Hanzi.find(@review.hanzi_id)
+      if @answer == @hanzi.character
+        validate(@review)
+      else
+        to_fail << @review.id
+      end        
+    end
+    @hanzi = Hanzi.find_by(id: @review.hanzi_id)
+    if to_fail.length == 0
+      flash[:success] = encourage
+      @next = pick_review(current_user.reviews.first)
+      if !@next.nil? and Time.now > @next.due
+        redirect_to review_path(@next.id)
+      else 
+        redirect_to reviews_path
+      end
+    else
+      redirect_to :action => "fail", :to_fail => to_fail, :id => @review.id
     end
   end
 
+  def fail
+    @to_fail = params[:to_fail]
+  end
+
   def fail_action
-    @review = current_user.reviews.find_by(id: params[:id])
-    @hanzi = Hanzi.find_by(id: @review.hanzi_id)
-    if params[:antedate] == 'yes'
-      Hanzi.where('components LIKE ?', "%#{@hanzi.character}%").each do |h|
+    params.keys.grep(/to_fail_id/).each do |c|
+      @review_to_fail = Review.find(c.gsub('to_fail_id', ''))
+      @review_to_fail.increment(:failed)
+      @review_to_fail.due = Time.now
+      @review_to_fail.save
+    end
+
+    params.keys.grep(/char/).each do |c|
+      @hanzi_to_fail = Hanzi.find_by(character: c.gsub('char', ''))
+      @review = current_user.reviews.find_by(hanzi_id: @hanzi_to_fail.id)
+      if !@review.nil?
+        @review.due = Time.now
+        @review.save
+      end
+    end
+
+    params.keys.grep(/antedate/).each do |a|
+      @parent_review = Review.find(a.gsub('antedate', ''))
+      @parent_hanzi = Hanzi.find(@parent_review.hanzi_id)
+      Hanzi.where('components LIKE ?', "%#{@parent_hanzi.character}%").each do |h|
         @child_review = current_user.reviews.where("hanzi_id = ?", h.id).first
         if !@child_review.nil?
           @child_review.due = Time.now
@@ -76,44 +119,12 @@ class ReviewsController < ApplicationController
         end
       end
     end
-    @hanzi.components.each_char do |c|
-      if params["#{c}"] == '1'
-        @parent_review = current_user.reviews.find_by(hanzi_id: Hanzi.find_by(character: c).id)
-        if !@parent_review.nil?
-          @parent_review.due = Time.now
-          @parent_review.save
-        end
-      end
-    end
-    @review.increment(:failed)
-    @review.due = Time.now
-    @review.save
-    @next = pick_review(current_user.reviews.first)
-    redirect_to @next
-  end
 
-  def validate
     @review = current_user.reviews.find_by(id: params[:id])
     @hanzi = Hanzi.find_by(id: @review.hanzi_id)
-    @rating = params[:rating]
-    @factor = case @rating
-              when "2" then 1
-              when "3" then 2
-              when "4" then 4
-              end
-    @interval = (Time.now - @review.updated_at).to_i() * @factor
-    if @interval < 1.day
-      @interval = 10.hours
-    end
-    @interval += (2*rand-1) * 0.1 * @interval
-    @review.due = Time.now + @interval
-    @review.save
+
     @next = pick_review(current_user.reviews.first)
-    if !@next.nil? and Time.now > @next.due
-      redirect_to review_path(@next.id)
-    else 
-      redirect_to reviews_path
-    end
+    redirect_to @next
   end
 
   def update
@@ -179,4 +190,21 @@ class ReviewsController < ApplicationController
         return nil
       end
     end
+
+    def validate(review)
+      @hanzi = Hanzi.find_by(id: review.hanzi_id)
+      @factor = 2
+      @interval = (Time.now - review.updated_at).to_i() * @factor
+      if @interval < 1.day
+        @interval = 10.hours
+      end
+      @interval += (2*rand-1) * 0.1 * @interval
+      review.due = Time.now + @interval
+      review.save
+    end
+
+    def encourage
+      return ["That's totally right!", "Yes!", "Correct!", "Right!", "True!"].sample
+    end
+
 end
