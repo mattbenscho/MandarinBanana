f0409ff21721ba2913eba816a0cb919178d7d13d
Matthias Schönborn <matthias@nandu.net>
2014-12-10 21:14:00 +0100

    Adding edit subtitles view

--- app/controllers/subtitles_controller.rb
+++ app/controllers/subtitles_controller.rb
@@ -1,5 +1,8 @@
 class SubtitlesController < ApplicationController
+  before_action :admin_user, only: [:edit, :update, :destroy]
+
   def show
+    store_location
     @subtitle = Subtitle.find(params[:id])
     @comments = @subtitle.comments
     @comment = current_user.comments.build if signed_in?
@@ -8,5 +11,30 @@ class SubtitlesController < ApplicationController
     @vocabulary = @subtitle.vocabulary
     @pinyin = @subtitle.pinyin
   end
+
+  def edit
+    @subtitle = Subtitle.find(params[:id])
+  end
+
+  def update
+    if current_user.admin?
+      @subtitle = Subtitle.find(params[:id])
+      @subtitle.sentence = params[:subtitle][:sentence]
+      @vocabulary_s = `lib/assets/find_word_definitions.rb "#{params[:subtitle][:words]}"`
+      @vocabulary = eval(@vocabulary_s)
+      @subtitle.vocabulary = @vocabulary
+      @pinyin_s = `lib/assets/find_pinyin.rb "#{params[:subtitle][:words]}"`
+      @pinyin = eval(@pinyin_s)
+      @subtitle.pinyin = @pinyin
+      @subtitle.save!
+    end
+    redirect_back_or root_url
+  end
+
+  private
+
+    def admin_user
+      redirect_back_or root_url unless current_user.admin? && !current_user.nil?
+    end
 end
 
--- spec/requests/subtitle_pages_spec.rb
+++ spec/requests/subtitle_pages_spec.rb
@@ -15,22 +15,52 @@ describe "SubtitlePages" do
 
   subject { page }
   
+  before { visit subtitle_path(@subtitle) }
+
   describe "subtitle page" do
     
     let!(:c1) { FactoryGirl.create(:comment, user: @user, subtitle: @subtitle, content: "Foo", hanzi: nil) }
     let!(:c2) { FactoryGirl.create(:comment, user: @user, subtitle: @subtitle, content: "Bar", hanzi: nil) }
     
     before { visit subtitle_path(@subtitle) }
-
+    
     it "should have the subtitle sentence as title" do
       expect(page).to have_title(@subtitle.sentence)
     end
-    # it { should have_selector('title', text: @subtitle.sentence) }
 
     describe "comments" do
       it { should have_content(c1.content) }
       it { should have_content(c2.content) }
       it { should have_content(@subtitle.comments.count) }
+    end
+  end
+
+  describe "editing" do
+    describe "as normal user" do
+      it { should_not have_content("edit subtitle") }
+    end
+    describe "as admin" do
+      let(:admin) { FactoryGirl.create(:admin) }
+
+      before do
+        sign_in admin
+        visit subtitle_path(@subtitle)
+      end
+
+      it { should have_link("edit subtitle", href: edit_subtitle_path(@subtitle)) }
+
+      describe "editing" do
+        before do
+          click_link "edit subtitle"
+          fill_in "Words", with: "大"
+          click_button "Save changes"
+        end
+
+        it { should have_content "大" }
+        it { should have_content "Definitions" }
+
+      end
+
     end
   end
 end
