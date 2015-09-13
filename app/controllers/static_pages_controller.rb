45e51aee227aa0ac3f3fb4d0b9f676cc95df705a
Matthias Schönborn <mbs@mailbox.org>
2015-09-13 22:28:27 +0200

    Refine statistics on home

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,8 +1,14 @@
 class StaticPagesController < ApplicationController
   def home
-    @next = (Hanzi.joins(:subtitles).uniq - Hanzi.includes(:mnemonics).where.not(:mnemonics => { id: nil }))[0..15]
-    @mnemonic_count_7_days = Mnemonic.where("created_at >= ?", 7.days.ago).count
-    @mnemonic_count_today = Mnemonic.where("created_at >= ?", Time.zone.now.beginning_of_day).count
+    @hanzis_with_subtitles = Hanzi.joins(:subtitles).uniq
+    @hanzis_with_mnemonics = Hanzi.includes(:mnemonics).where.not(:mnemonics => { id: nil })
+    @to_be_covered = @hanzis_with_subtitles - @hanzis_with_mnemonics
+    @to_be_covered_count = @to_be_covered.count
+    @next = @to_be_covered[0..15]
+    @mnemonic_total_count = Mnemonic.all.count
+    @mnemonic_uniq_count = Hanzi.joins(:mnemonics).uniq.count
+    @mnemonic_count_7_days = Hanzi.includes(:mnemonics).where(:mnemonics => { created_at: [7.days.ago..Time.zone.now] }).count
+    @mnemonic_count_today = Hanzi.includes(:mnemonics).where(:mnemonics => { created_at: [Time.zone.today.beginning_of_day..Time.zone.now] }).count
   end
 
   def about
