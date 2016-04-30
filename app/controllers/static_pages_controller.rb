aad34f6805703c57c03c029f21c4914eeabed416
Matthias Schönborn <mbs@mailbox.org>
2016-04-30 13:14:16 +0200

    select the next ones from all Hanzi, not only the ones with subtitles

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,9 +2,8 @@ class StaticPagesController < ApplicationController
   def home
     @hanzis_with_subtitles = Hanzi.joins(:subtitles).uniq
     @hanzis_with_mnemonics = Hanzi.includes(:mnemonics).where.not(:mnemonics => { id: nil })
-    @to_be_covered = @hanzis_with_subtitles - @hanzis_with_mnemonics
-    @to_be_covered_count = @to_be_covered.count
-    @next = @to_be_covered[0..15]
+    @to_be_covered_count = (@hanzis_with_subtitles - @hanzis_with_mnemonics).count
+    @next = (Hanzi.all - @hanzis_with_mnemonics)[0..15]
     @mnemonic_total_count = Mnemonic.all.count
     @mnemonic_uniq_count = Hanzi.joins(:mnemonics).uniq.count
     @mnemonic_count_7_days = Hanzi.includes(:mnemonics).where(:mnemonics => { created_at: [7.days.ago..Time.zone.now] }).count
