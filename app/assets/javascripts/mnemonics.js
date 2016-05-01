539e71796747914132496b0aa73ff64bf3d138ff
Matthias Schönborn <mbs@mailbox.org>
2016-05-01 16:22:46 +0200

    Insert snippet at cursor position!

--- app/assets/javascripts/mnemonics.js
+++ app/assets/javascripts/mnemonics.js
@@ -1,7 +1,31 @@
-function addSnippet(event) {
-    var snippet = event.target || event.srcElement;
-    if (snippet == "[object HTMLLIElement]") {
-	var to_add = snippet.textContent || snippet.innerText;
-	document.getElementById("mnemonic_aide").value += "(" + to_add + ")";
-    }
+function saveCursorPosition() {
+    var textarea = document.getElementById('mnemonic_aide');
+    CursorCaretPos = textarea.selectionStart;
 }
+
+function insertAtCursor(event) {
+    var textarea = document.getElementById('mnemonic_aide');
+    var snippet = event.target || event.srcElement;
+    var to_add = snippet.textContent || snippet.innerText;
+    var to_add = "(" + to_add + ")";
+    //IE support
+    if (document.selection) {
+        textarea.focus();
+        sel = document.selection.createRange();
+        sel.text = to_add;
+    }
+    //MOZILLA and others
+    else if (textarea.selectionStart || textarea.selectionStart == '0') {
+        var startPos = textarea.selectionStart;
+        var endPos = textarea.selectionEnd;
+        textarea.value = textarea.value.substring(0, startPos)
+            + to_add
+            + textarea.value.substring(endPos, textarea.value.length);
+        textarea.selectionStart = startPos + to_add.length;
+        textarea.selectionEnd = startPos + to_add.length;
+    } else {
+        textarea.value += to_add;
+    }
+    textarea.focus();
+}
+
